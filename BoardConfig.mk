274e31c6a30ef97de6cad0828591917e73855d9e
Giulio Cervera <giulio.cervera@gmail.com>
2012-02-11 14:17:52 +0100

    e739: initial T-Mobile LG myTouch import
    based on e730

---
+++ BoardConfig.mk
@@ -0,0 +1,4 @@
+-include device/lge/victor-common/BoardConfigCommon.mk
+
+TARGET_PREBUILT_KERNEL := device/lge/e739/prebuilt/kernel
+TARGET_PREBUILT_RECOVERY_KERNEL := device/lge/e739/prebuilt/kernel
---
+++ e739.mk
@@ -0,0 +1,22 @@
+$(call inherit-product, device/lge/victor-common/victor.mk)
+
+# The gps config appropriate for this device
+PRODUCT_COPY_FILES += device/common/gps/gps.conf_US:system/etc/gps.conf
+
+# Inherit non-open-source blobs.
+$(call inherit-product-if-exists, vendor/lge/e739/e739-vendor.mk)
+
+ifeq ($(TARGET_PREBUILT_KERNEL),)
+LOCAL_KERNEL := device/lge/e739/prebuilt/kernel
+else
+LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
+endif
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_KERNEL):kernel \
+    device/lge/e739/prebuilt/wireless.ko:system/lib/modules/wireless.ko \
+    device/lge/e739/prebuilt/fbconsole.ko:system/lib/modules/fbconsole.ko
+
+PRODUCT_NAME := e739
+PRODUCT_DEVICE := e739
+PRODUCT_MODEL := T-Mobile LG myTouch
