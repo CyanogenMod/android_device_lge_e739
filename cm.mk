8ed1f3ae3bfd43040a5bba5625a551151dc8c16f
Giulio Cervera <giulio.cervera@gmail.com>
2012-04-26 02:49:38 +0200

    e739: update fingerprint
    Change-Id: I410882c7af6a4f925b72a0bc3d20c8b5b8e6111b

--- cm.mk
+++ cm.mk
@@ -8,7 +8,7 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/gsm.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=lge_victor BUILD_FINGERPRINT=lge/lge_victor/victor:2.3.4/GRJ22/V10a-OCT-05-2011.2ED317DCA9:user/release-keys PRIVATE_BUILD_DESC="lge_victor-user 2.3.4 GRJ22 V10a-OCT-05-2011.2ED317DCA9 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=e739 BUILD_FINGERPRINT=lge/e739/e739:2.3.4/GRJ22/V10k-Oct-11-2011.2ED318C776:user/release-keys PRIVATE_BUILD_DESC="e739 2.3.4 GRJ22 V10k-Oct-11-2011.2ED318C776 release-keys"
 
 PRODUCT_NAME := cm_e739
 PRODUCT_DEVICE := e739
